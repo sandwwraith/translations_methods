909a35a4c01c95cf82d9c8e4826deac36c215121
sandwwraith <sandwwraith@gmail.com>
2017-04-06 02:13:41 +0300

    CLI

--- lab2v6/src/main/kotlin/top/sandwwraith/mt/lab2v6/App.kt
+++ lab2v6/src/main/kotlin/top/sandwwraith/mt/lab2v6/App.kt
@@ -1,6 +1,8 @@
 @file:JvmName("App")
+
 package top.sandwwraith.mt.lab2v6
 
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
 
@@ -10,11 +12,30 @@ import java.nio.file.Paths
  * 		  ITMO University, 2017
  **/
 
-fun helloWorldGenerator() = "Hello, world!"
+fun executeDot(name: String) =
+        ProcessBuilder("dot", "-Tsvg", "$name.gv")
+                .redirectOutput(File("$name.svg"))
+                .redirectError(ProcessBuilder.Redirect.INHERIT)
+                .start()
+                .waitFor()
 
 fun main(args: Array<String>) {
-    val t = Parser.parse("int *a;")
-    Files.newBufferedWriter(Paths.get("kek.gv")).use {
-        TreePrinter(t).printTo(it)
+    if (args.isEmpty()) {
+        System.err.println("Please specify input string as command-line argument and optional output name as second argument")
+        return
+    }
+    val name: String? = if (args.size == 1) null else args[1]
+
+    val tree = Parser.parse(args[0])
+    if (name != null) {
+        Files.newBufferedWriter(Paths.get("$name.gv")).use {
+            TreePrinter(tree, name).printTo(it)
+        }
+        val code = executeDot(name)
+        if (code != 0) System.err.println("Dot has non-zero exit status, maybe something went wrong")
+    } else {
+        System.out.bufferedWriter().use {
+            TreePrinter(tree).printTo(it)
+        }
     }
 }
