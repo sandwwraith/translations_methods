f8adbfce41b24d4c9d33c5a7ee07172f73aff847
sandwwraith <sandwwraith@gmail.com>
2017-06-08 00:47:00 +0300

    Fix multiproject configuration, add application plugin

---
+++ lab3v7/src/main/kotlin/top/sandwwraith/mt/lab3v7/App.kt
@@ -0,0 +1,13 @@
+@file:JvmName("App")
+
+package top.sandwwraith.mt.lab3v7
+
+/**
+ * @author Leonid Startsev
+ *		  sandwwraith@gmail.com
+ * 		  ITMO University, 2017
+ **/
+
+fun main(args: Array<String>) {
+    println("Hello, lab 3!")
+}
